8d9dbfa21de43014aa495abdc615221a5ee38a49
Guillaume <guillaume.thouvenin@vates.tech>
2026-01-10 20:44:11 +0100

    update

--- maps.ml
+++ maps.ml
@@ -1,43 +1,48 @@
+(* https://cs3110.github.io/textbook/chapters/ds/hash_tables.html *)
 module type Map = sig
-
-  (** [('k, 'v) t] is the type of maps that bind keys of type
-      ['k] to values of type ['v]. *)
   type ('k, 'v) t
+  (** [('k, 'v) t] is the type of maps that bind keys of type ['k] to values of
+      type ['v]. *)
 
-  (** [insert k v m] is the same map as [m], but with an additional
-      binding from [k] to [v].  If [k] was already bound in [m],
-      that binding is replaced by the binding to [v] in the new map. *)
   val insert : 'k -> 'v -> ('k, 'v) t -> ('k, 'v) t
+  (** [insert k v m] is the same map as [m], but with an additional binding from
+      [k] to [v]. If [k] was already bound in [m], that binding is replaced by
+      the binding to [v] in the new map. *)
 
-  (** [find k m] is [Some v] if [k] is bound to [v] in [m],
-      and [None] if not. *)
   val find : 'k -> ('k, 'v) t -> 'v option
+  (** [find k m] is [Some v] if [k] is bound to [v] in [m], and [None] if not.
+  *)
 
-  (** [remove k m] is the same map as [m], but without any binding of [k].
-      If [k] was not bound in [m], then the map is unchanged. *)
   val remove : 'k -> ('k, 'v) t -> ('k, 'v) t
+  (** [remove k m] is the same map as [m], but without any binding of [k]. If
+      [k] was not bound in [m], then the map is unchanged. *)
 
-  (** [empty] is the empty map. *)
   val empty : ('k, 'v) t
+  (** [empty] is the empty map. *)
 
-  (** [of_list lst] is a map containing the same bindings as
-      association list [lst].
-      Requires: [lst] does not contain any duplicate keys. *)
   val of_list : ('k * 'v) list -> ('k, 'v) t
+  (** [of_list lst] is a map containing the same bindings as association list
+      [lst]. Requires: [lst] does not contain any duplicate keys. *)
 
-  (** [bindings m] is an association list containing the same
-      bindings as [m]. There are no duplicates in the list. *)
   val bindings : ('k, 'v) t -> ('k * 'v) list
+  (** [bindings m] is an association list containing the same bindings as [m].
+      There are no duplicates in the list. *)
 end
 
+(* https://ocaml.org/docs/lists *)
+(* https://ocaml.org/manual/5.4/api/List.html *)
 module ListMap : Map = struct
-        type ('k, 'v) t = unit
+  type ('k, 'v) t = ('k * 'v) list
 
-        let insert _ _ _ = failwith "insert not implemented"
-        let find  _ _ = failwith "find not implemented"
-        let remove _ _ = failwith "remove not implemented"
-        let empty  = failwith "empty not implemented"
-        let of_list  _ = failwith "of_list not implemented"
-        let bindings _ = failwith "bindings not implemented"
-
+  (* *)
+  let insert k v m = (k, v) :: m
+  let find k m = List.assoc_opt k m
+  let remove k m = List.filter (fun (k', _) -> k <> k') m
+  let empty = []
+  let of_list l = l
+  let bindings m = List.fold_left (fun acc (k, v) -> 
+          match List.find_opt (fun (k',_) -> k' = k) acc with
+          | None -> insert k v acc
+          | Some(_) -> raise (Failure "Duplicated keys")
+  ) [] m
 end
